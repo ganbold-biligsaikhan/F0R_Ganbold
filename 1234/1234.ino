1348bd89382e5b45ee8a1eafb220d0dcec35e263
Ganbold Biligsaikhan <bilgee_merci@yahoo.com>
2017-10-12 16:32:44 +0800

    Initial commit

---
+++ 1234/1234.ino
@@ -0,0 +1,16 @@
+int ThermistorPin = 0;
+int Vo;
+float R1 = 10000;
+float logR2, R2, T;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+char K;
+
+
+
+void loop() {
+ Serial.write(K);
+ Serial.println(K);
+  
+ 
+
+}
